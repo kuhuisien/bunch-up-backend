97104dd5ed40af9b3bab8f5f1e4997d726cc00c9
kuhuisien <kuhuisien@hotmail.com>
2022-08-02 22:33:52 +0800

    refine table column

--- src/main/java/com/springboot/bunch/entity/Bunch.java
+++ src/main/java/com/springboot/bunch/entity/Bunch.java
@@ -20,9 +20,19 @@ public class Bunch {
     @Column(name = "title", nullable = false)
     private String title;
 
+    @Column(name = "subtitle", nullable = false)
+    private String subtitle;
+
     @Column(name = "description", nullable = false)
     private String description;
 
-    @Column(name = "content", nullable = false)
-    private String content;
+    @Column(name = "imageUrl", nullable = false)
+    private String imageUrl;
+
+    @Column(name = "address", nullable = false)
+    private String address;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+    
 }
