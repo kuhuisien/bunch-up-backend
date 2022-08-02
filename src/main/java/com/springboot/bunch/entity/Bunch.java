c6725fd9bca1e149588ddae04e8de8c30ac7511a
kuhuisien <kuhuisien@hotmail.com>
2022-08-02 22:26:01 +0800

    create jpa entity bunch

---
+++ src/main/java/com/springboot/bunch/entity/Bunch.java
@@ -0,0 +1,28 @@
+package com.springboot.bunch.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table(name="bunches", uniqueConstraints = {@UniqueConstraint(columnNames = {"title"})})
+public class Bunch {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+}
