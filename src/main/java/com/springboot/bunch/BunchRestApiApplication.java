b96a33ab09df70e9d9df0f9af2d51c64a6aca8f2
kuhuisien <kuhuisien@hotmail.com>
2022-08-02 21:00:58 +0800

    initial commit

---
+++ src/main/java/com/springboot/bunch/BunchRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.bunch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BunchRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BunchRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/bunch/BunchRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.bunch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BunchRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
