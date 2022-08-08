6b38350da2f3a4b00c680170e59983ad750ce742
kuhuisien <kuhuisien@hotmail.com>
2022-08-08 16:34:43 +0800

    add validation handling

--- src/main/java/com/springboot/bunch/controller/BunchController.java
+++ src/main/java/com/springboot/bunch/controller/BunchController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -21,7 +22,7 @@ public class BunchController {
     }
 
     @PostMapping
-    public ResponseEntity<BunchDto> createBunch(@RequestBody BunchDto bunchDto) {
+    public ResponseEntity<BunchDto> createBunch(@Valid @RequestBody BunchDto bunchDto) {
         return new ResponseEntity<>(bunchService.createBunch(bunchDto), HttpStatus.CREATED);
     }
 
--- src/main/java/com/springboot/bunch/payload/BunchDto.java
+++ src/main/java/com/springboot/bunch/payload/BunchDto.java
@@ -11,23 +11,22 @@ import javax.validation.constraints.Size;
 public class BunchDto {
     private Long id;
 
-    @NotEmpty
     @Size(min = 2, message = "Title should have at least 2 characters")
     private String title;
 
-    @NotEmpty
     @Size(min = 2, message = "Subtitle should have at least 2 characters")
     private String subtitle;
 
-    @NotEmpty
+    @NotEmpty(message = "Description should not be null or empty")
     private String description;
 
-    @NotEmpty
+    @NotEmpty(message = "Image url should not be null or empty")
     private String imageUrl;
 
-    @NotEmpty
+    @NotEmpty(message = "Address should not be null or empty")
     private String address;
 
-    @Email
+    @NotEmpty(message = "Email should not be null or empty")
+    @Email(message = "Email is invalid")
     private String email;
 }
