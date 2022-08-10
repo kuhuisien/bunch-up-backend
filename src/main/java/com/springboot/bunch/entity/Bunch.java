e5ab2483cef720fbf73ce6ef9d4d5b094f6d5216
kuhuisien <kuhuisien@hotmail.com>
2022-08-10 22:49:22 +0800

    add created_by field for Bunch entity

--- src/main/java/com/springboot/bunch/entity/Bunch.java
+++ src/main/java/com/springboot/bunch/entity/Bunch.java
@@ -3,8 +3,9 @@ package com.springboot.bunch.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+import org.hibernate.annotations.CreationTimestamp;
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
@@ -38,5 +39,9 @@ public class Bunch {
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
+
+    @Column(updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
     
 }
